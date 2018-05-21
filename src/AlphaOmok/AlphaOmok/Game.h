9ab4c7f78715d6f367818f2da38c5a6183b4826e
Hosung <holnet1026@gmail.com>
2018-05-22 01:41:49 +0900

    [Updated] Omok win or lose logic implement

--- src/AlphaOmok/AlphaOmok/Game.h
+++ src/AlphaOmok/AlphaOmok/Game.h
@@ -3,7 +3,16 @@
 #include <vector>
 #include "Common.h"
 #include "Subject.h"
+#include "OmokUtil.h"
+
 using namespace std;
+
+typedef struct Lines {
+	vector<char> leftUp;
+	vector<char> rightUp;
+	vector<char> horizontal;
+	vector<char> vertical;
+} Lines;
 
 class Game:
 	public Subject
@@ -13,15 +22,23 @@ private:
 	char m_CurrentTurn;
 	vector<Piece> m_History;
 	char** m_Board;
+	int m_Status;
+
 public:
 	Game();
 	~Game();
 	//Âø¼ö
 	bool PutPiece(int row, int col);
+	//½Â¸®Ã¼Å© 
+	bool winCheck(int row, int col);
 	//ÅÏº¯°æ
 	void reserveTurn();
+	char markPiece(Point x, Point current);
+	void printVector(vector<char> line);
+	Lines GetLines(int row, int col);
 	char** GetBoard();
 	char GetCurrentTurn();
+	int GetStatus();
 	vector<Piece> GetHistory();
 };
 
